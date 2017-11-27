fdc474c9423cf9b1f7bd838ecfdb638b213fe7c0
Lukas Ruzicka <lukas.ruzicka96@seznam.cz>
2017-11-27 11:03:18 +0100

    Icons for items added

--- src/gui/BagContent.java
+++ src/gui/BagContent.java
@@ -1,10 +1,21 @@
 package gui;
 
+import commontale.Main;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.util.Callback;
 import logic.Bag;
 import util.Observer;
+import javafx.scene.control.*;
 
 import java.util.Set;
 
@@ -24,6 +35,8 @@ public class BagContent extends ListView implements Observer{
         bagData.remove(0,bagData.size());
         Set<String> bagCont = subject.getBagContent();
         for (String item : bagCont) {
+            if(item.equals("key1"))
+                item = "key";
             bagData.add(item);
         }
     }
@@ -31,6 +44,40 @@ public class BagContent extends ListView implements Observer{
     private void init(){
         bagData = FXCollections.observableArrayList();
         setItems(bagData);
+        setCellFactory(new Callback<ListView, ListCell>() {
+            @Override
+            public ListCell call(ListView param) {
+                return new XCell();
+            }
+        });
         update();
+    }
+
+    static class XCell extends ListCell<String>{
+
+        private HBox hBox = new HBox();
+        private ImageView icon = new ImageView();
+        private Label text = new Label("");
+
+        public XCell(){
+            text.setFont(Font.font("Sans", FontWeight.SEMI_BOLD,15));
+            text.setAlignment(Pos.CENTER);
+            text.setPadding(new Insets(0,0,0,10));
+            hBox.getChildren().addAll(icon,text);
+        }
+
+        @Override
+        protected void updateItem(String item, boolean empty){
+            super.updateItem(item,empty);
+            setText(null);
+            if(empty)
+                setGraphic(null);
+            else{
+                text.setText(item);
+                icon.setImage(new Image(Main.class.getResourceAsStream("/sources/"+item+".png"),
+                        25,25,false,true));
+                setGraphic(hBox);
+            }
+        }
     }
 }
